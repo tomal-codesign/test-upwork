b2731c9a5cf0bd960a6fecbb1faba91e8a448c5e
Tomal Ahmed Raj <sj.tomalahmedraj@gmail.com>
2025-10-20 12:13:26 +0600

    new

--- src/app/tour-details/tour-details.ts
+++ src/app/tour-details/tour-details.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// import Navigation from 'swiper/modules/navigation/navigation.js';
+// import Pagination from 'swiper/modules/pagination/pagination.js';
+import Swiper from 'swiper/bundle';
+import 'swiper/css';
+import 'swiper/swiper-bundle.css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 
 @Component({
@@ -7,12 +15,34 @@ import { Component } from '@angular/core';
   templateUrl: './tour-details.html',
   styleUrl: './tour-details.scss'
 })
-export class TourDetails {
+export class TourDetails implements AfterViewInit {
+  @ViewChild('swiperContainer', { static: false }) swiperRef!: ElementRef;
   selectedCurrency = 'USD';
   setCurrency(currency: string) {
     this.selectedCurrency = currency;
 
   }
 
+  ngAfterViewInit() {
+    new Swiper(this.swiperRef.nativeElement, {
+      // modules: [Navigation, Pagination],
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop: true,
+      // navigation: {
+      //   nextEl: '.swiper-button-next-custom',
+      //   prevEl: '.swiper-button-prev-custom',
+      // },
 
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        640: { slidesPerView: 1, spaceBetween: 10 },
+        768: { slidesPerView: 2, spaceBetween: 15 },
+        1024: { slidesPerView: 4, spaceBetween: 20 }
+      }
+    });
+  }
 }
---
+++ src/swiper-angular.d.ts
@@ -0,0 +1,5 @@
+import { ModuleWithProviders } from '@angular/core';
+
+export declare class SwiperModule {
+    static forRoot(): ModuleWithProviders<SwiperModule>;
+}
